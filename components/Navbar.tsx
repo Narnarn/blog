e9ce116d2ec3cd618d61482f295f6887c401f9b6
Narnarn <keyra32@gmail.com>
2022-06-08 21:11:58 +0800

    test for mode switcher

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -41,6 +41,86 @@ const Navbar = () => {
   useEffect(() => {
     setMounted(true);
   }, []);
+  if (!mounted) {
+    return (
+      <>
+        <nav className="sticky top-0 z-6  border-b border-gray-300 bg-[#fffaf5] bg-opacity-70 backdrop-filter backdrop-blur-lg font-Montserrat">
+          <div className="flex px-8 justify-between md:justify-around pt-4  pb-2 text-gray-600 ">
+            <Link href="/">
+              <a className="text-lg font-bold min-w-max mr-4">
+                {/* <FcGlobe className="inline mr-1 -mt-1 hover:animate-spin" /> */}
+                Space
+              </a>
+            </Link>
+
+            <div className="hidden md:flex justify-center md:justify-end  mt-1">
+              {navigations.map((n) => (
+                <Link href={n.link} key={n.link}>
+                  <a className="navitem">{n.name}</a>
+                </Link>
+              ))}
+              {mounted && (
+                <button
+                  onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                  className="inline ml-4 "
+                >
+                  {theme == "dark" && <MdOutlineLightMode size={20} />}
+                  {theme == "light" && <MdOutlineDarkMode size={20} />}
+                </button>
+              )}
+            </div>
+
+            <div className="inline-block md:hidden pt-1">
+              <Menu as="div" className="relative inline-block mr-4">
+                <Menu.Button>
+                  {({ open }) => (
+                    <>
+                      <FcMenu
+                        className={`${open ? "hidden" : "block"}`}
+                        size={20}
+                      />
+                      <VscChevronDown
+                        className={`${open ? "block" : "hidden"}`}
+                      />
+                    </>
+                  )}
+                </Menu.Button>
+
+                {/* Use the Transition component. */}
+                <Transition
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Menu.Items className="absolute right-0 grid grid-rows-4 bg-[#fffaf5]/70 backdrop-filter backdrop-blur-lg shadow-md px-2 py-1 border-[1px] border-gray-300 text-gray-700 divide-y divide-gray-300 divide-dashed ">
+                    {navigations.map((n) => (
+                      <Menu.Item key={n.link}>
+                        <MyLink href={n.link}>
+                          <div>{n.name}</div>
+                        </MyLink>
+                      </Menu.Item>
+                    ))}
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+              {mounted && (
+                <button
+                  onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                  className=""
+                >
+                  {theme == "dark" && <MdOutlineLightMode size={20} />}
+                  {theme == "light" && <MdOutlineDarkMode size={20} />}
+                </button>
+              )}
+            </div>
+          </div>
+        </nav>
+      </>
+    );
+  }
 
   return (
     <>
@@ -59,15 +139,14 @@ const Navbar = () => {
                 <a className="navitem">{n.name}</a>
               </Link>
             ))}
-            {mounted && (
-              <button
-                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                className="inline ml-4 "
-              >
-                {theme == "dark" && <MdOutlineLightMode size={20} />}
-                {theme == "light" && <MdOutlineDarkMode size={20} />}
-              </button>
-            )}
+
+            <button
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              className="inline ml-4 "
+            >
+              {theme == "dark" && <MdOutlineLightMode size={20} />}
+              {theme == "light" && <MdOutlineDarkMode size={20} />}
+            </button>
           </div>
 
           <div className="inline-block md:hidden pt-1">
@@ -106,15 +185,14 @@ const Navbar = () => {
                 </Menu.Items>
               </Transition>
             </Menu>
-            {mounted && (
-              <button
-                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                className=""
-              >
-                {theme == "dark" && <MdOutlineLightMode size={20} />}
-                {theme == "light" && <MdOutlineDarkMode size={20} />}
-              </button>
-            )}
+
+            <button
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              className=""
+            >
+              {theme == "dark" && <MdOutlineLightMode size={20} />}
+              {theme == "light" && <MdOutlineDarkMode size={20} />}
+            </button>
           </div>
         </div>
       </nav>
