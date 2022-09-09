d2040fcd6b43d7aeeec2f693ced39cfbb9d7fd5e
Narnarn <keyra32@gmail.com>
2022-09-09 11:10:17 +0800

    preload fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,11 @@ class MyDocument extends Document {
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Charis+SIL:wght@400;700&family=DM+Mono:wght@300;400;500&family=Inter:wght@400;500;700&family=Montserrat:ital,wght@0,400;0,500;0,700;1,500&family=Noto+Serif+SC:wght@300;400;500;600&family=Work+Sans:wght@300;400;500&display=swap"
+            rel="preload"
+            as="style"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Charis+SIL:wght@400;700&family=DM+Mono:wght@300;400;500&family=Inter:wght@400;500;700&family=Montserrat:ital,wght@0,400;0,500;0,700;1,500&family=Noto+Serif+SC:wght@300;400;500;600&family=Work+Sans:wght@300;400;500&display=swap"
             rel="stylesheet"
           />
         </Head>
